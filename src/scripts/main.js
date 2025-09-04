01930bd081ab0d0f3bf71dfc41d14d2bdf84be7f
anastasiiadns <nastyadenisenko17@gmail.com>
2025-09-04 17:58:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const elementsThead = document.querySelectorAll('table');
+
+for (const table of elementsThead) {
+  const elementsTheadTr = table.querySelector('thead tr');
+  const stringsTheadTh = elementsTheadTr.querySelectorAll('th');
+  const cloneTheadTh = stringsTheadTh[1].cloneNode(true);
+
+  elementsTheadTr.insertBefore(cloneTheadTh, elementsTheadTr.lastElementChild);
+
+  const elementsTbodyTr = table.querySelectorAll('tbody tr');
+
+  for (const row of elementsTbodyTr) {
+    const stringsTbodyTd = row.querySelectorAll('td');
+
+    if (stringsTbodyTd.length >= 2) {
+      const cloneTbodyTd = stringsTbodyTd[1].cloneNode(true);
+
+      row.insertBefore(cloneTbodyTd, row.lastElementChild);
+    }
+  }
+
+  const elementsTfootTr = table.querySelector('tfoot tr');
+  const stringsTfootTh = elementsTfootTr.querySelectorAll('th');
+  const cloneTfootTh = stringsTfootTh[1].cloneNode(true);
+
+  elementsTfootTr.insertBefore(cloneTfootTh, elementsTfootTr.lastElementChild);
+}
